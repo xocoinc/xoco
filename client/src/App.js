af85e15210467379432b9c3e24546fef56ea4b0c
Carver <kerenketepela@gmail.com>
2022-02-02 19:45:41 +0300

    [FEAT]: render the Header Component

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/header/Header";
+
 function App() {
   return (
-    <div className="App">
-      <h1>This is Xoco</h1>
+    <div className="app">
+      <Header/>
     </div>
   );
 }
