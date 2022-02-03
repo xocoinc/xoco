f7d2e44958f83fa58081917c0502eb98cc2c93c4
Carver <kerenketepela@gmail.com>
2022-02-03 19:08:10 +0300

    bring in login component

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/header/Header";
+import Login from "./components/login/Login";
 
 function App() {
   return (
     <div className="app">
       <Header/>
+      <Login />
     </div>
   );
 }
