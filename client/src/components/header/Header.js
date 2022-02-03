8560b24a2f8405140408e7c6d95fbbc676f5d7c8
Carver <kerenketepela@gmail.com>
2022-02-03 19:09:12 +0300

    add the sell button

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -3,14 +3,16 @@ import SearchIcon from '@mui/icons-material/Search';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import HelpOutlineIcon from '@mui/icons-material/HelpOutline';
+import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
 import './Header.css'
+import logo from './xoco blue (2).png'
 
 const Header = () =>{
     return(
         <nav className='header'>
             <a href ='/home'>
                 <img 
-                src = 'https://avatars.githubusercontent.com/u/98540847?s=400&u=62abf6625f4b2b664e9dc2114315af6fbe8f9e2f&v=4'
+                src = {logo}
                 alt = ''
                 className='nav__logo'
                 />
@@ -19,7 +21,6 @@ const Header = () =>{
                 <input type='text' className='search' placeholder='Search items' />
                 <SearchIcon className='search__icon'/>
             </div>
-            {/* nav__links */}
             <div className='nav__links'>
                 <div className='nav__options'>
                     <AccountCircleIcon className='optionlineOne' />
@@ -27,15 +28,19 @@ const Header = () =>{
                 </div>
                 <div className='nav__options'>
                     <HelpOutlineIcon className='optionlineOne' />
-                    <span className='optionlineTwo'>Help</span>
+                    <span className='optionlineTwo'>Help</span> 
                 </div>
                 <div className='nav__options'>
-                    <a href='/'>
                     <AddShoppingCartIcon className='optionlineOne' />
                     <span className='optionlineTwo'>Cart</span>
-                    </a>
                 </div>
-            </div>
+                <div className='sell__button'>
+                    <span className='sell__buttoncontent'>
+                        <AttachMoneyIcon className='sell__icon' />
+                        <h5>Sell</h5>
+                    </span>
+                </div>
+            </div> 
         </nav>
     )
 }
