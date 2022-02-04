5bc7977ac5f530299ca4093ea1ae62f4d06f93ef
Lizzie A <lizzie>
2022-02-04 13:37:33 +0300

    feat: create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,22 @@
 import Header from "./components/header/Header";
 import Login from "./components/login/Login";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Account from "./components/account/Account";
 
 function App() {
   return (
     <div className="app">
-      <Header/>
-      <Login />
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/account">
+            <Account />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
