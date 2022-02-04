6dc3ed96b2aecc04788a30532d56e73fe295f28b
Lizzie A <lizzie>
2022-02-04 13:36:56 +0300

    feat: create an acount component

---
+++ client/src/components/account/Account.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Account.css";
+
+const Account = () => {
+  return (
+    <div className="account">
+      <img
+        src="http://localhost:3000/static/media/xoco%20blue%20(2).f5ba87df822711d4de53.png"
+        alt=""
+      />
+      <div className="accountButtons">
+        <div className="login">
+          <Link to="/login" className="login">
+            <span>SignIn</span>
+          </Link>
+        </div>
+        <div className="register">
+          <Link to="/register" className="register">
+            <span>SignUp</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Account;
