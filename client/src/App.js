57a0dccda46260bbc67b4481d8913c61e68c4ec0
Carver <kerenketepela@gmail.com>
2022-02-06 15:44:43 +0300

    feat: routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,11 @@
+import Home from "./components/home/Home";
 import Header from "./components/header/Header";
 import Login from "./components/login/Login";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Account from "./components/account/Account";
+import Cart from "./components/cart/Cart";
+import Sell from "./components/sell/Sell";
+import Help from "./components/help/Help";
 
 function App() {
   return (
@@ -9,11 +13,23 @@ function App() {
       <Router>
         <Header />
         <Switch>
+          <Route path='/help'>
+            <Help />
+          </Route>
+          <Route path='/sell'>
+            <Sell />
+          </Route>
+          <Route path='/cart'>
+            <Cart />
+          </Route>
           <Route path="/account">
             <Account />
           </Route>
           <Route path="/login">
             <Login />
+          </Route>
+          <Route path='/'>
+            <Home />
           </Route>
         </Switch>
       </Router>
