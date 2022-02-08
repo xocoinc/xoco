dd85354a22c8d659df5a62f507185d118d4dfb7c
Carver <kerenketepela@gmail.com>
2022-02-08 12:03:07 +0300

    feat:use mongodb

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
 const express = require("express")
+const dotenv = require("dotenv")
+
+dotenv.config()
 
 //init app
 const app = express()
 
+//db
+require('./config/db')
+
 //pull in routes
+const auth = require("./routes/auth")
 const user = require("./routes/user")
 
 //middleware
-app.use("/user", user)
+app.use(express.json())
+app.use("/api/auth", auth)
+app.use("/api/user", user)
 
 
-const port = 3002
+const port = process.env.PORT ||5000
 app.listen(port, () =>console.log(`server runing on ${port}`))
