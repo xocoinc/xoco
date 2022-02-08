c1e6d5198adbf52384193d98160f83493635c376
Carver <kerenketepela@gmail.com>
2022-02-08 12:07:09 +0300

    chore:initial setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
 // import Home from "./components/home/Home";
-import Header from "./components/header/Header";
-import Login from "./components/login/Login";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Account from "./components/account/Account";
-import Cart from "./components/cart/Cart";
-import Sell from "./components/sell/Sell";
-import Help from "./components/help/Help";
-import Slider from "./components/slider/Slider";
+import Header from "./components/header/Header"
+import Login from "./components/login/Login"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+import Account from "./components/account/Account"
+import Cart from "./components/cart/Cart"
+import Sell from "./components/sell/Sell"
+import Help from "./components/help/Help"
+import Slider from "./components/slider/Slider"
 
 function App() {
   return (
@@ -14,13 +14,13 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path='/help'>
+          <Route path="/help">
             <Help />
           </Route>
-          <Route path='/sell'>
+          <Route path="/sell">
             <Sell />
           </Route>
-          <Route path='/cart'>
+          <Route path="/cart">
             <Cart />
           </Route>
           <Route path="/account">
@@ -29,13 +29,13 @@ function App() {
           <Route path="/login">
             <Login />
           </Route>
-          <Route path='/'>
+          <Route path="/">
             <Slider />
           </Route>
         </Switch>
       </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
