9fe424262f5012a54f6123517dc0ed374c49278b
Carver <kerenketepela@gmail.com>
2022-02-13 20:05:41 +0300

    feat:create product details page

--- client/src/components/checkout/ProductDescription.js
+++ client/src/components/checkout/ProductDescription.js
@@ -1,7 +1,32 @@
-const ProductDescription = () => {
+import "./ProductDescription.css"
+
+const ProductDescription = ({ item }) => {
   return (
     <div className="product_description">
-      <h1>Description</h1>
+      <div className="image-container">
+        <img src={`${process.env.PUBLIC_URL}/assets/images/lp3.png`} alt="" />
+        <h2>recomended items</h2>
+        <div className="recomended_items">
+          <img src={`${process.env.PUBLIC_URL}/assets/images/lp1.png`} alt="" />
+          <img src={`${process.env.PUBLIC_URL}/assets/images/lp1.png`} alt="" />
+          <img src={`${process.env.PUBLIC_URL}/assets/images/lp1.png`} alt="" />
+        </div>
+      </div>
+      <div className="product_info">
+        {/* heading - product title */}
+        <h1>Hp Pavillion</h1>
+        {/* product description text */}
+        <p>
+          The HP Pavilion line of laptops is known for its speed and
+          portability, as well as its reasonable prices. ... As a lightweight
+          device with a luminous display and a long battery life, this series of
+          HP laptops can handle whatever you throw at it while being delightful
+          to use
+        </p>
+        {/* Add to cart button */}
+        <button>Add to cart</button>
+        {/* recomented items */}
+      </div>
     </div>
   )
 }
