78fd8e15c1508a5ee7432766162c595c6b3f89be
Carver <kerenketepela@gmail.com>
2022-02-22 14:12:13 +0300

    feat: intergration,check if user is logged in

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
-import Header from "./components/header/Header";
-import Login from "./components/login/Login";
-import Signup from "./components/signup/Signup";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Account from "./components/account/Account";
-import Cart from "./components/cart/Cart";
-import Sell from "./components/sell/Sell";
-import Help from "./components/help/Help";
+import Header from "./components/header/Header"
+import Login from "./components/login/Login"
+import Signup from "./components/signup/Signup"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom"
+import Account from "./components/account/Account"
+import Cart from "./components/cart/Cart"
+import Sell from "./components/sell/Sell"
+import Help from "./components/help/Help"
 // import Slider from "./components/slider/Slider"
-import Categories from "./components/home/Categories";
-import Products from "./components/home/Products";
-import ProductDescription from "./components/checkout/ProductDescription";
-import Checkout from "./components/checkout/Checkout";
+import Categories from "./components/home/Categories"
+import Products from "./components/home/Products"
+import ProductDescription from "./components/checkout/ProductDescription"
+import Checkout from "./components/checkout/Checkout"
+import Product from "./components/home/Product"
 
 function App() {
+  const user = false
   return (
     <div className="app">
       <Router>
@@ -33,19 +40,22 @@ function App() {
           <Route path="/account">
             <Account />
           </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
+          <Route path="/login">{user ? <Redirect to="/" /> : <Login />}</Route>
           <Route path="/signup">
-            <Signup />
+            {user ? <Redirect to="/" /> : <Signup />}
           </Route>
           <Route path="/checkout">
             <Checkout />
           </Route>
+          <Route path="/product/:categories">
+            <Products />
+          </Route>
+          <Route path="/product/:id">
+            <Products />
+          </Route>
           <Route path="/">
             {/* <Slider /> */}
             <Categories />
-            <Products />
           </Route>
         </Switch>
       </Router>
