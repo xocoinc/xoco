1c41b8db7cd6d5cccc1817eeaee8b7920ab8c5f5
Peter Tepela <kerenketepela@gmail.com>
2022-02-22 08:52:37 +0300

    fix:merge error

--- client/src/components/checkout/Checkout.js
+++ client/src/components/checkout/Checkout.js
@@ -2,42 +2,6 @@ import "./Checkout.css";
 import { Link } from "react-router-dom";
 
 const Checkout = () => {
-    return ( <
-        div className = "checkout" >
-        <
-        h1 > Checkout < /h1> <
-        form >
-        <
-        div class = "c-details" >
-        <
-        h4 > Name < /h4> <
-        input type = "text"
-        placeholder = "First name" / >
-        <
-        input type = "text"
-        placeholder = "Last name" / >
-        <
-        h4 > Mpesa Phone Number < /h4> <
-        input type = "tel"
-        placeholder = "+2547..." / >
-        <
-        h4 > Email < /h4> <
-        input type = "email"
-        placeholder = "email" / >
-        <
-        h4 > Password < /h4> <
-        input type = "password"
-        placeholder = "password" / >
-        <
-        /div> <
-        Link to = "/address" >
-        <
-        button type = "submit" > Confirm order < /button> <
-        /Link> <
-        /form> <
-        /div>
-    );
-
   return (
     <div className="checkout">
       <h1>Checkout</h1>
@@ -54,9 +18,7 @@ const Checkout = () => {
           <h4>Password</h4>
           <input type="password" placeholder="password" />
         </div>
-        <Link to="/address">
-          <button type="submit" class="c-button" >Proceed</button>
-        </Link>
+         <button type="submit" class="c-button" >Proceed</button>
       </form>
     </div>
   );
