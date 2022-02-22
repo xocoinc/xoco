d885214a9b6692a337f24a4ae0e1c63ac4ff5473
Carver <kerenketepela@gmail.com>
2022-02-22 14:13:05 +0300

    feat: fetch product from db

--- client/src/components/home/Product.jsx
+++ client/src/components/home/Product.jsx
@@ -1,6 +1,10 @@
-import React from "react"
+import { React }from "react"
 import "./Product.css"
 import { Link } from 'react-router-dom'
+// import { publicRequest } from "../publicRequest";
+// import { useDispatch } from 'react-redux'
+// import { addProduct } from "../../redux/cartRedux";
+
 
 // import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 // import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
