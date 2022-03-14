f291d38e4226545f93a3623aac4299428416c6d6
Carver <kerenketepela@gmail.com>
2022-03-14 13:15:19 +0300

    feat: fetch users

---
+++ client/src/redux/apiCalls.js
@@ -0,0 +1,12 @@
+import { loginFailure, loginStart, loginSuccess } from "./userRedux"
+import axios from "axios"
+
+export const login = async (dispatch, user) => {
+  dispatch(loginStart())
+  try {
+    const res = await axios.post("http://localhost:5000/api/auth/login", user)
+    dispatch(loginSuccess(res.data))
+  } catch (err) {
+    dispatch(loginFailure())
+  }
+}
