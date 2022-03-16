a3eb2ab8534d9b269eaba3eee30d1ada1d806046
Carver <kerenketepela@gmail.com>
2022-03-16 11:49:49 +0300

    feat:(config) firebase storage

---
+++ client/src/firebase.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app"
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBZDBuY74UEq22SdxhGTNYBWuBuRbZfPqU",
+  authDomain: "xoco-b7306.firebaseapp.com",
+  projectId: "xoco-b7306",
+  storageBucket: "xoco-b7306.appspot.com",
+  messagingSenderId: "648563388821",
+  appId: "1:648563388821:web:bc64fe09483a522952069c",
+  measurementId: "G-EJFWXP0S7B",
+}
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig)
+export default app
