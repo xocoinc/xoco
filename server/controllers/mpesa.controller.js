55a97342fdc934c1ab8448416c1e8c2849498932
Carver <kerenketepela@gmail.com>
2022-03-17 12:35:41 +0300

    feat:get verification token

--- server/controllers/mpesa.controller.js
+++ server/controllers/mpesa.controller.js
@@ -11,12 +11,32 @@ const shortCode = process.env.SHORT_CODE
 const newPass = () => {
   const date = datetime.create()
   const format = date.format("YmdHMS")
-  const passString = shortCode + passKey + format
+  const passString = consumerSecret + comsumerKey + format
   const encodedPass = Buffer.from(passString).toString("base64")
 
   return encodedPass
 }
+exports.token = () => {
+  let unirest = require("unirest")
+  let req = unirest("GET", "https://sandbox.safaricom.co.ke/oauth/v1/generate")
 
+  req.query({
+    grant_type: "client_credentials",
+  })
+
+  req.headers({
+    Authorization:
+      "Basic SWVjN0huU25DN0w0enVpMG42ZmdMZUdXYVQ4Q09qcTJNNlkzOFZQVUtiSkVwMVJtMjAyMjAzMTcxMjMzMjE= ",
+  })
+
+  req.end(res => {
+    if (res.error) throw new Error(res.error)
+    console.log(res.body)
+  })
+}
 exports.getMpesaPassword = async (req, res) => {
   res.status(200).json({ Pass: newPass() })
+}
+exports.stkPush = (req, res) => {
+  res.send("cool")
 }
--- server/routes/mpesa.js
+++ server/routes/mpesa.js
@@ -1,7 +1,12 @@
 const express = require("express")
-const { getMpesaPassword } = require("../controllers/mpesa.controller")
+const {
+  getMpesaPassword,
+  stkPush,
+  token,
+} = require("../controllers/mpesa.controller")
 const router = express.Router()
 
 router.route("/password").get(getMpesaPassword)
+router.route("/stkpush").post(token, stkPush)
 
 module.exports = router
