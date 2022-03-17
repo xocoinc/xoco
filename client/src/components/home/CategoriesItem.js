0046730e7551a02739c4fd519fbdacda955b569a
Carver <kerenketepela@gmail.com>
2022-03-17 17:00:12 +0300

    fix:wrap the categories div with Link

--- client/src/components/home/CategoriesItem.js
+++ client/src/components/home/CategoriesItem.js
@@ -1,14 +1,18 @@
 import "./CategoriesItem.css"
+import { Link } from "react-router-dom"
 
 const CategoriesItem = ({ item }) => {
+  console.log(item)
   return (
-    <div className="categories__item">
-      <img src={item.img} alt="img" />
-      <div className="info">
-        <h1>{item.title}</h1>
-        <button>Shop Now</button>
+    <Link to={`/products/${item.cat}`}>
+      <div className="categories__item">
+        <img src={item.img} alt="img" />
+        <div className="info">
+          <h1>{item.title}</h1>
+          <button>Shop Now</button>
+        </div>
       </div>
-    </div>
+    </Link>
   )
 }
 
